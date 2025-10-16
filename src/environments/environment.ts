7c7970e8e20a7706230ba4ad73480fae8d885fa2
Juangogut <kpperez@tecnocomfenalco.edu.co>
2025-10-16 17:25:36 -0500

    update-images3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
   pokedexGithub: 'https://github.com/keilermora/pokedex-angular',
 
   // Images folder url
-  imagesPath: 'assets/images',
+  imagesPath: '/assets/images',
 
   // Default language
   language: 'en',
